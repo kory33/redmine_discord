408a9cb5b3e20fcb20e8c20bf8c17922dcacc654
kory33 <6561358+kory33@users.noreply.github.com>
2017-11-16 05:51:35 +0100

    Initialize project

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_discord do
+  name 'Redmine Discord plugin'
+  author 'kory'
+  description 'Send notifications to discord channels in a rich format.'
+  version '0.0.1'
+  url 'https://github.com/kory33/redmine_discord'
+  author_url 'https://github.com/kory33'
+end
