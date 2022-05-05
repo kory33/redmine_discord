0c76a6e1e56861a9a0590c850d1f9e65c5890c12
inductor <kela@inductor.me>
2022-05-05 10:32:44 +0900

    Support Rails 6 for Redmine 5

--- init.rb
+++ init.rb
@@ -1,11 +1,10 @@
 require 'redmine'
-require_dependency 'redmine_discord/listener'
 
 Redmine::Plugin.register :redmine_discord do
   name 'Redmine Discord plugin'
   author 'kory'
   description 'Send notifications to discord channels in a rich embed format.'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/kory33/redmine_discord'
   author_url 'https://github.com/kory33'
 
@@ -17,3 +16,8 @@ Redmine::Plugin.register :redmine_discord do
     partial: 'settings/redmine_discord'
   })
 end
+
+if Rails.configuration.respond_to?(:autoloader) && Rails.configuration.autoloader == :zeitwerk
+  Rails.autoloaders.each { |loader| loader.ignore(File.dirname(__FILE__) + '/lib') }
+end
+require File.dirname(__FILE__) + '/lib/redmine_discord'
